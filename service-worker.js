4d61a4013f088431655ec353e37b0f87cfae42e9
Fajar Jayadi <89061075+efjayadi@users.noreply.github.com>
2026-03-28 10:03:43 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,37 @@
 const CACHE_NAME = 'jestudios-v1';
-const ASSETS = [
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install Service Worker
+// Install event: Caches the essential app shell assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Activate & Cleanup
+// Activate event: Cleans up any outdated caches from previous versions
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
       );
     })
   );
 });
 
-// Fetch logic
+// Fetch event: Serves assets from cache if available, otherwise goes to the network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
